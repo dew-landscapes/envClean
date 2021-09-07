9566e344fecc2fc1ea662210a79c50ced95f4f94
Nigel Willoughby <nige.willoughby@gmail.com>
2021-09-07 11:37:34 +0930

    added over_ride options to `filter_spat_rel()`

--- R/envClean.R
+++ R/envClean.R
@@ -636,11 +636,12 @@
 #' @param df Dataframe.
 #' @param dist_col Character. Name of the column containing the spatial
 #' reliability.
-#' @param default Numeric. Default value for `dist_col` to use if `dist_col` is
-#' `NA`. Useful when importing reliable data with no associated reli
 #' @param dist Numeric. Filter spatial reliability greater than this value. In
 #' the same units as `dist_col`.
 #' @param context Character. column names defining the context.
+#' @param over_ride Named list. List names must be the same as column names. Any
+#' names in `over_ride` will be matched to column names in `df` and any values
+#' in that list element will not be filtered (on spatial reliability).
 #'
 #' @return Dataframe with records of greater than `dist` filtered. Filtering is
 #' done at `context` level.
@@ -649,18 +650,33 @@
 #' @examples
   filter_spat_rel <- function(df
                               , dist_col = "rel_dist"
-                              , default = Inf
                               , dist = 50
                               , context
+                              , over_ride = NULL
                               ){
 
-    vis_rel <- df %>%
-      dplyr::mutate(dist_col := dplyr::if_else(is.na(!!ensym(dist_col))
-                                               , default
-                                               , !!ensym(dist_col)
-                                               )
-                    ) %>%
+    .df <- df
+
+    if(isTRUE(!is.null(over_ride))) {
+
+      for(i in seq_along(over_ride)) {
+
+        col <- names(over_ride)[[i]]
+
+        .df <- .df %>%
+          dplyr::mutate(!!ensym(dist_col) := if_else(!!ensym(col) %in% over_ride[[i]]
+                                                , dist
+                                                , !!ensym(dist_col)
+                                                )
+                        )
+
+      }
+
+    }
+
+    vis_rel <- .df %>%
       dplyr::distinct(dplyr::across(any_of(context)),!!ensym(dist_col)) %>%
+      dplyr::filter(!is.na(!!ensym(dist_col))) %>%
       dplyr::filter(!!ensym(dist_col) <= dist) %>%
       dplyr::select(-!!ensym(dist_col))
 
