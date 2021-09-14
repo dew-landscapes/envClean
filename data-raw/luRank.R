e309dc8373cc6af260b8b7072565a394829a8439
Acanthiza <nigel.willoughby@sa.gov.au>
2021-09-14 17:36:30 +0930

    more `lurank` fixing

--- data-raw/luRank.R
+++ data-raw/luRank.R
@@ -3,16 +3,16 @@
 
   lurank <- tibble::tribble(
     ~rank, ~sort
-    , "Kingdom", 1
-    , "Phylum", 2
-    , "Class", 3
-    , "Order", 4
-    , "Family", 5
-    , "Genus", 6
-    , "Species", 7
-    , "Subspecies", 8
-    , "Variety", 9
-    , "Form", 10
+    , "kingdom", 1
+    , "phylum", 2
+    , "class", 3
+    , "order", 4
+    , "family", 5
+    , "genus", 6
+    , "species", 7
+    , "subspecies", 8
+    , "variety", 9
+    , "form", 10
     ) %>%
     dplyr::mutate(rank = forcats::fct_reorder(rank,sort)
                   , rank = factor(rank,ordered = TRUE)
