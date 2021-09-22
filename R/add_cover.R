83576f56702d4596d7bb51fd21ed28ecf73b170c
Acanthiza <nigel.willoughby@sa.gov.au>
2021-09-22 14:06:11 +0930

    snake_case in `add_cover`

--- R/add_cover.R
+++ R/add_cover.R
@@ -4,57 +4,57 @@
 #'
 #' @param df Dataframe with context, taxa and cover columns.
 #' @param context Character. Name of columns defining context.
-#' @param covcol Character. Name of column containing cover values.
-#' @param smallcov A small cover value assigned to any record for which there
+#' @param cov_col Character. Name of column containing cover values.
+#' @param small_cov A small cover value assigned to any record for which there
 #' is no available site, pca or taxa cover value.
 #'
-#' @return Dataframe with covcol removed and replaced with best guess cover in
+#' @return Dataframe with cov_col removed and replaced with best guess cover in
 #' column use_cover
 #' @export
 #'
 #' @examples
   add_cover <- function(df
                         , context = "cell"
-                        , covcol = "use_cover"
-                        , smallcov = 0.009
+                        , cov_col = "use_cover"
+                        , small_cov = 0.009
                         ) {
 
-    namesdf <- c(names(df)[names(df) != covcol],"use_cover")
+    namesdf <- c(names(df)[names(df) != cov_col], cov_col)
 
-    sitecov <- df %>%
-      dplyr::group_by(across(contains("cutpc")),across(all_of(context)),taxa) %>%
-      dplyr::summarise(sitecover = max(!!ensym(covcol))) %>%
+    site_cov <- df %>%
+      dplyr::group_by(across(contains("cut_pc")),across(all_of(context)),taxa) %>%
+      dplyr::summarise(site_cover = max(!!ensym(cov_col))) %>%
       dplyr::ungroup()
 
-    pcacov <- df %>%
-      dplyr::group_by(across(contains("cutpc")),taxa) %>%
-      dplyr::summarise(pcacover = median(!!ensym(covcol), na.rm = TRUE)) %>%
+    pca_cov <- df %>%
+      dplyr::group_by(across(contains("cut_pc")),taxa) %>%
+      dplyr::summarise(pca_cover = median(!!ensym(cov_col), na.rm = TRUE)) %>%
       dplyr::ungroup()
 
-    taxacov <- df %>%
+    taxa_cov <- df %>%
       dplyr::group_by(taxa) %>%
-      dplyr::summarise(taxacover = median(!!ensym(covcol), na.rm = TRUE)) %>%
+      dplyr::summarise(taxa_cover = median(!!ensym(cov_col), na.rm = TRUE)) %>%
       dplyr::ungroup()
 
     df %>%
-      dplyr::left_join(sitecov) %>%
-      dplyr::left_join(pcacov) %>%
-      dplyr::left_join(taxacov) %>%
-      dplyr::mutate(use_cover = if_else(!is.na(sitecover)
-                                   ,sitecover
-                                   ,if_else(!is.na(pcacover)
-                                            , pcacover
-                                            , taxacover
+      dplyr::left_join(site_cov) %>%
+      dplyr::left_join(pca_cov) %>%
+      dplyr::left_join(taxa_cov) %>%
+      dplyr::mutate(use_cover = if_else(!is.na(site_cover)
+                                   ,site_cover
+                                   ,if_else(!is.na(pca_cover)
+                                            , pca_cover
+                                            , taxa_cover
                                             )
                                    )
-                    , use_cover = if_else(is.na(use_cover),smallcov,use_cover)
+                    , use_cover = if_else(is.na(use_cover),small_cov,use_cover)
                     ) %>%
       dplyr::select(all_of(names(df))) %>%
       # remove sites where all cover values had to be assigned as a small value
       dplyr::group_by(across(all_of(context))) %>%
       dplyr::mutate(covmean = mean(use_cover)) %>%
       dplyr::ungroup() %>%
-      dplyr::filter(covmean != smallcov) %>%
+      dplyr::filter(covmean != small_cov) %>%
       dplyr::select(all_of(namesdf))
 
     }
