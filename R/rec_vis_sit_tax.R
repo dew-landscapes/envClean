c5ef03189775331b285e864246a917bd3cbca3f6
Acanthiza <nigel.willoughby@sa.gov.au>
2021-11-03 17:44:06 +1030

    got `find_taxa` working

---
+++ R/rec_vis_sit_tax.R
@@ -0,0 +1,25 @@
+#' How many records, visits, sites and taxa in a dataframe
+#'
+#' @param df Dataframe with taxa records.
+#' @param site_cols Name of column(s) defining sites.
+#' @param visit_cols Name of column(s) defining visits.
+#' @param taxa_cols Name of column(s) defining taxa.
+#'
+#' @return One row dataframe
+#' @export
+#'
+#' @examples
+rec_vis_sit_tax <- function(df
+                            , site_cols
+                            , visit_cols
+                            , taxa_cols
+                            ) {
+
+  df %>%
+    dplyr::summarise(taxa = n_distinct(across(any_of(taxa_cols)))
+                     , records = nrow(.)
+                     , visits = n_distinct(across(any_of(site_cols)))
+                     , sites = n_distinct(across(any_of(visit_cols)))
+                     )
+
+}
