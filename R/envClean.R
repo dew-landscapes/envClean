ca23ef8901ecca9600633617169e2fe5212f700c
Acanthiza <nigel.willoughby@sa.gov.au>
2023-01-10 15:39:38 +1030

    Deal with indigenous status a little better

--- R/envClean.R
+++ R/envClean.R
@@ -657,7 +657,8 @@
 #' @param df Dataframe with taxa column and column indicating indigenous
 #' status.
 #' @param taxa_col Character. Name of column with taxa.
-#' @param ind_col Character. Name of column with indigenous status
+#' @param ind_col Character. Name of column with indigenous status (assumed to
+#' be, largely, `Y` or `N`).
 #'
 #' @return Dataframe with unique taxa and their indigenous status.
 #' @export
@@ -667,7 +668,7 @@
 
     df %>%
       dplyr::count(dplyr::across(!!rlang::ensym(taxa_col)),dplyr::across(!!rlang::ensym(ind_col))) %>%
-      dplyr::filter(!is.na(!!rlang::ensym(ind_col))) %>%
+      dplyr::filter(!!rlang::ensym(ind_col) %in% c("Y", "N")) %>%
       dplyr::group_by(!!rlang::ensym(taxa_col)) %>%
       dplyr::filter(n == max(n, na.rm = TRUE)) %>%
       dplyr::ungroup() %>%
