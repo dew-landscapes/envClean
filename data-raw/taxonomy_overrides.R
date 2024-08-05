e276fa1dfb9c15b85e5af0843aca1a5eacd7517c
Joel Allan <JAllan@env.sa.gov.au>
2024-08-05 09:47:32 +0930

    Added Sminthopsis overrides

--- data-raw/taxonomy_overrides.R
+++ data-raw/taxonomy_overrides.R
@@ -5,5 +5,7 @@
 
 taxonomy_overrides <- tibble::tribble(
   ~original_name, ~taxa_to_search, ~use_species, ~use_subspecies, ~note,
-  "Charadrius rubricollis", "Thinornis cucullatus", "Thinornis cucullatus", NA, "Hooded Plover"
+  "Charadrius rubricollis", "Thinornis cucullatus", "Thinornis cucullatus", NA, "Hooded Plover",
+  "Sminthopsis fuliginosus aitkeni","Sminthopsis fuliginosa aitkeni","Sminthopsis fuliginosa","Sminthopsis fuliginosa aitkeni","KI Dunnart", # using "fuliginosus" not "fuliginosa" returns incorrect taxa
+  "Sminthopsis fuliginosus","Sminthopsis fuliginosa","Sminthopsis fuliginosa", NA,"EP/KI Dunnarts", # using "fuliginosus" not "fuliginosa" returns incorrect taxa
 )
