b09d076226020327f71090ca85cbe141f1a0ea6e
Nigel <36317413+Acanthiza@users.noreply.github.com>
2024-08-14 18:59:18 +0930

    docs

--- data-raw/taxonomy_overrides.R
+++ data-raw/taxonomy_overrides.R
@@ -6,7 +6,7 @@
 taxonomy_overrides <- tibble::tribble(
   ~original_name, ~taxa_to_search, ~use_species, ~use_subspecies, ~note,
   "Charadrius rubricollis", "Thinornis cucullatus", "Thinornis cucullatus", NA, "Hooded Plover",
-  "Sminthopsis fuliginosus aitkeni","Sminthopsis fuliginosa aitkeni","Sminthopsis fuliginosa","Sminthopsis fuliginosa aitkeni","KI Dunnart", # using "fuliginosus" not "fuliginosa" returns incorrect taxa
-  "Sminthopsis fuliginosus","Sminthopsis fuliginosa","Sminthopsis fuliginosa", NA,"EP/KI Dunnarts", # using "fuliginosus" not "fuliginosa" returns incorrect taxa
-  "Gallirallus philippensis mellori","Hypotaenidia philippensis mellori","Hypotaenidia philippensis", "Hypotaenidia philippensis mellori"# from ALA: now regarded as Hypotaenidia philippensis. H.p.mellori is accepted, but galah::search_taxa reverts to species. original_name in
+  "Sminthopsis fuliginosus aitkeni", "Sminthopsis fuliginosa aitkeni", "Sminthopsis fuliginosa", "Sminthopsis fuliginosa aitkeni", "KI Dunnart", # using "fuliginosus" not "fuliginosa" returns incorrect taxa
+  "Sminthopsis fuliginosus", "Sminthopsis fuliginosa","Sminthopsis fuliginosa", NA, "EP/KI Dunnarts", # using "fuliginosus" not "fuliginosa" returns incorrect taxa
+  "Gallirallus philippensis mellori", "Hypotaenidia philippensis mellori", "Hypotaenidia philippensis", "Hypotaenidia philippensis mellori", NA_character_# from ALA: now regarded as Hypotaenidia philippensis. H.p.mellori is accepted, but galah::search_taxa reverts to species. original_name in
 )
