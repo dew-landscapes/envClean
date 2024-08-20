c26378ca63f433327dec0aa579de823d539d5c7c
Joel Allan <JAllan@env.sa.gov.au>
2024-08-20 15:46:08 +0930

    More taxonomy overrides

--- data-raw/taxonomy_overrides.R
+++ data-raw/taxonomy_overrides.R
@@ -8,5 +8,10 @@ taxonomy_overrides <- tibble::tribble(
   "Charadrius rubricollis", "Thinornis cucullatus", "Thinornis cucullatus", NA, "Hooded Plover",
   "Sminthopsis fuliginosus aitkeni", "Sminthopsis fuliginosa aitkeni", "Sminthopsis fuliginosa", "Sminthopsis fuliginosa aitkeni", "KI Dunnart", # using "fuliginosus" not "fuliginosa" returns incorrect taxa
   "Sminthopsis fuliginosus", "Sminthopsis fuliginosa","Sminthopsis fuliginosa", NA, "EP/KI Dunnarts", # using "fuliginosus" not "fuliginosa" returns incorrect taxa
-  "Gallirallus philippensis mellori", "Hypotaenidia philippensis mellori", "Hypotaenidia philippensis", "Hypotaenidia philippensis mellori", "Australian Buff-banded Rail" # from ALA: now regarded as Hypotaenidia philippensis. H.p.mellori is accepted, but galah::search_taxa reverts to species. original_name in
+  "Gallirallus philippensis mellori", "Hypotaenidia philippensis mellori", "Hypotaenidia philippensis", "Hypotaenidia philippensis mellori", "Australian Buff-banded Rail", # from ALA: now regarded as Hypotaenidia philippensis. H.p.mellori is accepted, but galah::search_taxa reverts to species. original_name in
+  "Grus rubicunda", "Antigone rubicunda","Antigone rubicunda",NA, "Brolga",
+  "Eucalyptus X paludicola", "Eucalyptus paludicola", "Eucalyptus paludicola", NA, "Marsh Gum/Mt Compass Swamp Gum",
+  "Eucalyptus x paludicola", "Eucalyptus paludicola", "Eucalyptus paludicola", NA, "Marsh Gum/Mt Compass Swamp Gum",
+  "Corybas X dentatus", "Corybas dentatus", "Corybas dentatus", NA, "Finniss/Toothed Helmet-orchid",
+  "Corybas x dentatus", "Corybas dentatus", "Corybas dentatus", NA, "Finniss/Toothed Helmet-orchid"
 )
