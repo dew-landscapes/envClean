d94ff67516b979655200b7bed87b1ea912bac1fb
Joel Allan <JAllan@env.sa.gov.au>
2025-07-28 16:07:26 +0930

    update taxa to search for MI Buff-banded Rail fix

--- data-raw/taxonomy_overrides.R
+++ data-raw/taxonomy_overrides.R
@@ -10,7 +10,7 @@ taxonomy_overrides <- tibble::tribble(
   "Sminthopsis fuliginosus aitkeni", "Sminthopsis fuliginosa aitkeni", "Sminthopsis fuliginosa", "Sminthopsis fuliginosa aitkeni", "KI Dunnart", # using "fuliginosus" not "fuliginosa" returns incorrect taxa
   "Sminthopsis fuliginosus", "Sminthopsis fuliginosa","Sminthopsis fuliginosa", NA, "EP/KI Dunnarts", # using "fuliginosus" not "fuliginosa" returns incorrect taxa
   "Gallirallus philippensis mellori", "Hypotaenidia philippensis mellori", "Hypotaenidia philippensis", "Hypotaenidia philippensis mellori", "Australian Buff-banded Rail", # from ALA: now regarded as Hypotaenidia philippensis. H.p.mellori is accepted, but galah::search_taxa reverts to species. original_name in
-  "Hypotaenidia philippensis macquariensis", "Hypotaenidia philippensis macquariensis", "Hypotaenidia philippensis", "Hypotaenidia philippensis mellori", "Australian Buff-banded Rail", # extinct Macquarie Island subsp, comes from obis
+  "Hypotaenidia philippensis macquariensis", "Hypotaenidia philippensis mellori", "Hypotaenidia philippensis", "Hypotaenidia philippensis mellori", "Australian Buff-banded Rail", # extinct Macquarie Island subsp, comes from obis
   "Grus rubicunda", "Antigone rubicunda","Antigone rubicunda",NA, "Brolga",
   "Eucalyptus X paludicola", "Eucalyptus paludicola", "Eucalyptus paludicola", NA, "Marsh Gum/Mt Compass Swamp Gum",
   "Eucalyptus x paludicola", "Eucalyptus paludicola", "Eucalyptus paludicola", NA, "Marsh Gum/Mt Compass Swamp Gum",
