81a8f365f0b6c96961052d0e0f783ef2b67f6db6
Acanthiza <nigel.willoughby@sa.gov.au>
2025-12-22 14:05:57 +1030

    default ht_func is median

--- R/add_height.R
+++ R/add_height.R
@@ -16,76 +16,78 @@
 #' @export
 #'
 #' @examples
-  add_height <- function(df
-                        , context = "cell"
-                        , env_prcomp
-                        , lustr
-                        , lustr_col = "ht"
-                        , ht_func = max
-                        ) {
+add_height <- function(df
+                      , context = "cell"
+                      , env_prcomp
+                      , lustr
+                      , lustr_col = "ht"
+                      , ht_func = "median"
+                      ) {
 
-    site_use_height <- df %>%
-      dplyr::left_join(lustr) %>%
-      dplyr::mutate(site_use_height = dplyr::case_when(if("height" %in% names(df)) !is.na(height) ~ height
-                                                       , !is.na(lifeform) ~ !!rlang::ensym(lustr_col)
-                                                       , TRUE ~ NA
-                                                       )
-                    ) %>%
-      dplyr::filter(!is.na(site_use_height)
-                    , site_use_height != 0
-                    ) %>%
-      dplyr::group_by(taxa
-                      , dplyr::across(tidyselect::any_of(context))
-                      ) %>%
-      dplyr::summarise(site_use_height = ht_func(site_use_height)) %>%
-      dplyr::ungroup()
+  site_use_height <- df |>
+    dplyr::left_join(lustr) |>
+    dplyr::mutate(site_use_height = dplyr::case_when(if("height" %in% names(df)) !is.na(height) ~ height
+                                                     , !is.na(lifeform) ~ !!rlang::ensym(lustr_col)
+                                                     , TRUE ~ NA
+                                                     )
+                  ) |>
+    dplyr::filter(!is.na(site_use_height)
+                  , site_use_height != 0
+                  ) |>
+    dplyr::group_by(taxa
+                    , dplyr::across(tidyselect::any_of(context))
+                    ) |>
+    dplyr::summarise(site_use_height = get(ht_func)(site_use_height
+                                                    , na.rm = TRUE
+                                                    )
+                     ) |>
+    dplyr::ungroup()
 
-    pca_use_height <- df %>%
-      dplyr::left_join(env_prcomp$pca_res_cell_cut) %>%
-      dplyr::distinct(taxa
-                      , dplyr::across(tidyselect::contains("cut_pc"))
-                      , dplyr::across(tidyselect::any_of(context))
-                      ) %>%
-      dplyr::left_join(site_use_height %>%
-                         dplyr::left_join(env_prcomp$pca_res_cell_cut) %>%
-                         dplyr::group_by(taxa
-                                         , dplyr::across(tidyselect::contains("cut_pc"))
-                                         ) %>%
-                         dplyr::summarise(pca_use_height = ht_func(site_use_height
-                                                                 , na.rm = TRUE
-                                                                 )
-                                          ) %>%
-                         dplyr::ungroup()
-                       )
+  pca_use_height <- df |>
+    dplyr::left_join(env_prcomp$pca_res_cell_cut) |>
+    dplyr::distinct(taxa
+                    , dplyr::across(tidyselect::contains("cut_pc"))
+                    , dplyr::across(tidyselect::any_of(context))
+                    ) |>
+    dplyr::left_join(site_use_height |>
+                       dplyr::left_join(env_prcomp$pca_res_cell_cut) |>
+                       dplyr::group_by(taxa
+                                       , dplyr::across(tidyselect::contains("cut_pc"))
+                                       ) |>
+                       dplyr::summarise(pca_use_height = get(ht_func)(site_use_height
+                                                                      , na.rm = TRUE
+                                                                      )
+                                        ) |>
+                       dplyr::ungroup()
+                     )
 
-    taxa_use_height <- site_use_height %>%
-      dplyr::group_by(taxa) %>%
-      dplyr::summarise(taxa_use_height = ht_func(site_use_height
-                                               , na.rm = TRUE
+  taxa_use_height <- site_use_height |>
+    dplyr::group_by(taxa) |>
+    dplyr::summarise(taxa_use_height = get(ht_func)(site_use_height
+                                                    , na.rm = TRUE
+                                                    )
+                     ) |>
+    dplyr::ungroup()
+
+  result <- df |>
+    dplyr::left_join(site_use_height) |>
+    dplyr::left_join(pca_use_height) |>
+    dplyr::left_join(taxa_use_height) |>
+    dplyr::mutate(use_height = dplyr::case_when(!is.na(site_use_height) ~ site_use_height
+                                               , !is.na(pca_use_height) ~ pca_use_height
+                                               , !is.na(taxa_use_height) ~ taxa_use_height
+                                               , TRUE ~ NA
                                                )
-                       ) %>%
-      dplyr::ungroup()
-
-    result <- df %>%
-      dplyr::left_join(site_use_height) %>%
-      dplyr::left_join(pca_use_height) %>%
-      dplyr::left_join(taxa_use_height) %>%
-      dplyr::mutate(use_height = dplyr::case_when(!is.na(site_use_height) ~ site_use_height
-                                                 , !is.na(pca_use_height) ~ pca_use_height
-                                                 , !is.na(taxa_use_height) ~ taxa_use_height
-                                                 , TRUE ~ NA
-                                                 )
-                    ) %>%
-      dplyr::select(tidyselect::all_of(names(df))
-                    , tidyselect::contains("cut_pc")
+                  ) |>
+    dplyr::select(tidyselect::all_of(names(df))
+                  , tidyselect::contains("cut_pc")
+                  , use_height
+                  ) |>
+    dplyr::distinct(taxa
+                    , dplyr::across(tidyselect::any_of(context))
                     , use_height
-                    ) %>%
-      dplyr::distinct(taxa
-                      , dplyr::across(tidyselect::any_of(context))
-                      , dplyr::across(tidyselect::contains("cut_pc"))
-                      , use_height
-                      )
+                    )
 
-    return(result)
+  return(result)
 
-  }
+}
