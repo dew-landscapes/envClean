f15d7cb92521cb28fded959ea27c8075f3ae340b
Simeon Zylinski <simeon.zylinski@sa.gov.au>
2026-05-15 15:49:51 +0930

    add report_clean - working with current 'core' inst/rmd/clean files

---
+++ R/report_clean.R
@@ -0,0 +1,37 @@
+#' Knit child documents to report the standard envCleaned process
+#'
+#' Note: some files include named R objects to output the currently-used settings, and the knit will
+#' error if these are missing. For example, the 'core' clean files require `scale` to describe
+#' extent, and  `settings_clean` for the fbd_dist setting. Set these in the report environment before using `report_clean`
+#'
+#' @param clean_summary Tibble containing names of clean process outputs (e.g. clean_start, bio_bin,
+#'   bio_clean, etc), As output from envTargets::summarise_store_object
+#' @param heading_level What level of subheading to set the output sections. E.g. default 2 will use
+#'   "##" to separate sections
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+#'
+report_clean <- function(clean_summary,
+                         heading_level = 2) {
+
+  for(file in clean_summary$rmd) {
+
+    child <- knitr::knit_child(
+      fs::path(system.file("rmd", package = "envClean"), file),
+      quiet = TRUE
+    )
+
+    cat(
+      paste0( #insert extra '#' to section headers
+        strrep("#", times = heading_level - 1),
+        gsub("^\\n", "", x = child),
+        sep = "\n\n"
+      )
+    )
+
+  }
+
+}
