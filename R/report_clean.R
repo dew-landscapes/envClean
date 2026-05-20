37b837c67834fcd5ea1bfb73c2d39811ac5690c7
Simeon Zylinski <simeon.zylinski@sa.gov.au>
2026-05-20 15:46:36 +0930

    docs

--- R/report_clean.R
+++ R/report_clean.R
@@ -1,8 +1,9 @@
 #' Knit child documents to report the standard envCleaned process
 #'
+#' Use with knitr chunk option `results="asis"`.
 #' Note: some files include named R objects to output the currently-used settings, and the knit will
 #' error if these are missing. For example, the 'core' clean files require `scale` to describe
-#' extent, and  `settings_clean` for the fbd_dist setting. Set these in the report environment before using `report_clean`
+#' extent, and  `settings_clean` for the fbd_dist setting. Set these in the report environment before using `report_clean`.
 #'
 #' @param clean_summary Tibble containing names of clean process outputs (e.g. clean_start, bio_bin,
 #'   bio_clean, etc), As output from envTargets::summarise_store_object
@@ -11,8 +12,6 @@
 #'
 #' @returns
 #' @export
-#'
-#' @examples
 #'
 report_clean <- function(clean_summary,
                          heading_level = 2) {
