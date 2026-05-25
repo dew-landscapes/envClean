97d7663a800e4b633deaee7410981731c75ed0b3
acanthiza <nigel.willoughby@sa.gov.au>
2026-05-25 18:17:35 +0930

    passing `scale` into knit_clean failed, so instead use the widely available `settings` list that should aready have both `extent` and `grain` elements

--- R/knit_clean.R
+++ R/knit_clean.R
@@ -1,24 +1,24 @@
 #' Knit child documents to report the standard envCleaned process
 #'
 #' Use with knitr chunk option `results="asis"`.
-#' Note: some files include named R objects to output the currently-used settings, and the knit will
-#' error if these are missing. For example, the 'core' clean files require `scale` to describe
-#' extent, and  `settings_clean` for the fbd_dist setting. Set these in the report environment before using `report_clean`.
+#' Note: some files include named R objects to output the currently-used
+#' settings, and the knit will error if these are missing. For example, the
+#' 'core' clean files require the list `settings` with elements `extent` and
+#' `grain`, and  `settings_clean` for the fbd_dist setting. Set these in the
+#' report environment before using `report_clean`.
 #'
-#' @param clean_summary Tibble containing names of clean process outputs (e.g. clean_start, bio_bin,
-#'   bio_clean, etc), As output from envTargets::summarise_store_object
-#' @param heading_level What level of subheading to set the output sections. E.g. default 2 will use
-#'   "##" to separate sections
-#' @param scale List with elements `extent` and `grain` as expected by many of
-#' the clean Rmd files. Default is to extract `scale` via
-#' `envFunc::extract_scale(element = basename(here::here()))`
+#' @param clean_summary Tibble containing names of clean process outputs (e.g.
+#' clean_start, bio_bin, bio_clean, etc), As output from
+#' `envTargets::summarise_store_object()`
+#' @param heading_level What level of subheading to set the output sections.
+#' e.g. default 3 will result in "###" heading level at the start of each clean
+#' Rmd
 #'
-#' @returns
+#' @returns as for `knitr::knit_child()`
 #' @export
 #'
 knit_clean <- function(clean_summary,
                          heading_level = 3
-                       , scale = envFunc::extract_scale(element = basename(here::here()))
                        ) {
 
   text <- NULL
