00655e41550701a04069c3208c98abcc431d2335
acanthiza <nigel.willoughby@sa.gov.au>
2026-06-30 15:00:36 +0930

    store argument option to find_taxa

--- R/find_taxa.R
+++ R/find_taxa.R
@@ -1,8 +1,5 @@
 #' Find how taxa changed through the cleaning/filtering/tidying process
 #'
-#' `find_taxa` does not work hierarchically. For example _Eucalyptus_ will only
-#' match genus level records, not species records, such as
-#' _Eucalyptus leucoxylon_.
 #'
 #' @param taxa_to_find Character. Taxa name to find.
 #' @param taxa_cols Character. Name of column(s) across data frames containing
@@ -11,6 +8,10 @@
 #' say, `taxonomy$species$lutaxa` as a result of `make_taxonomy()`.
 #' @param filt_df_prefix Character. Prefix used in each of the data frames
 #' created at each step in the filtering process.
+#' @param store Character. Path to a targets store. If available, use this as an
+#' alternative to ensuring the objects to be found using `filt_df_prefix` are in
+#' the global environment. Other than finding by `filt_df_prefix` objects in the
+#' store are also filtered by `type == "stem"` and `format == "parquet"`
 #'
 #' @return
 #' @export
@@ -18,56 +19,67 @@
 #' @example inst/examples/find_taxa_ex.R
 find_taxa <- function(taxa_to_find
                       , taxa_cols = c("original_name", "taxa")
+                      , context = NULL
                       , lutaxa
-                      , filt_df_prefix = "flor_"
+                      , filt_df_prefix = "^flor_|^bio_"
+                      , store = NULL
                       ) {
-
-  cols_out <- c("name", "taxas")
 
   find_string <- paste0(taxa_to_find, collapse = "|")
 
-  to_find <- lutaxa %>%
+  to_find <- lutaxa |>
     dplyr::filter(if_any(any_of(taxa_cols)
                          , ~ grepl(find_string, .)
                          )
-                  ) %>%
-    dplyr::select(any_of(taxa_cols)) %>%
-    as.matrix() %>%
-    as.vector() %>%
+                  ) |>
+    dplyr::select(any_of(taxa_cols)) |>
+    as.matrix() |>
+    as.vector() |>
     unique()
 
-  ls(pattern = filt_df_prefix
-     , envir = globalenv()
-     ) %>%
-    tibble::enframe(name = NULL, value = "name") %>%
-    dplyr::mutate(obj = purrr::map(name
-                                   , get
+  if(is.null(store)) {
+
+    dfs <- ls(pattern = filt_df_prefix
+              , envir = globalenv()
+              ) |>
+      tibble::enframe(name = NULL, value = "name") |>
+      dplyr::mutate(obj = purrr::map(name, get))
+
+
+  } else {
+
+    dfs <- targets::tar_meta(store = store) |>
+      dplyr::filter(grepl(filt_df_prefix, name)
+                    , type == "stem"
+                    , format == "parquet"
+                    ) |>
+      dplyr::select(name) |>
+      dplyr::mutate(obj = purrr::map(name
+                                     , \(x) arrow::open_dataset(fs::path(store, "objects", x)) |>
+                                       dplyr::select(tidyselect::any_of(c(taxa_cols, bin))) |>
+                                       dplyr::collect()
+                                     )
+                    )
+
+  }
+
+  dfs |>
+    dplyr::mutate(obj = purrr::map(obj
+                                   , \(x) x %>%
+                                     dplyr::select(tidyselect::any_of(c(taxa_cols, context))) %>%
+                                     dplyr::filter(dplyr::if_any(tidyselect::any_of(taxa_cols), \(x) x %in% to_find))
                                    )
-                  , is_df = purrr::map_lgl(obj
-                                           , \(x) "data.frame" %in% class(x)
-                                           )
-                  ) %>%
-    dplyr::filter(is_df) %>%
-    dplyr::filter(purrr::map_lgl(obj, \(x) ! "sf" %in% class(x))) %>%
-    dplyr::mutate(nrow = purrr::map_dbl(obj
-                                        , nrow
-                                        )
-                  ) %>%
-    dplyr::mutate(founds = purrr::map(obj
-                                      , \(x) x %>%
-                                        dplyr::select(tidyselect::any_of(taxa_cols)) %>%
-                                        dplyr::filter(dplyr::if_any(tidyselect::any_of(taxa_cols), \(x) x %in% to_find))
-                                      )
                   , taxa = taxa_to_find
-                  , records = purrr::map_dbl(founds, base::nrow)
-                  , found = purrr::map_chr(founds
-                                           , \(x) x %>%
-                                             dplyr::distinct() %>%
-                                             base::unlist() %>%
-                                             base::unique() %>%
+                  , records = purrr::map_dbl(obj, base::nrow)
+                  , found = purrr::map_chr(obj
+                                           , \(x) x |>
+                                             dplyr::select(tidyselect::any_of(taxa_cols)) |>
+                                             dplyr::distinct() |>
+                                             base::unlist() |>
+                                             base::unique() |>
                                              envFunc::vec_to_sentence()
                                            )
-                  ) %>%
-    dplyr::arrange(desc(nrow))
+                  ) |>
+    dplyr::arrange(desc(records))
 
 }
