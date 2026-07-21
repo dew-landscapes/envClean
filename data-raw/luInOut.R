96567eea1c16091494cf2122ccd6c25369b1d77f
Calamanthus <joel.allan@sa.gov.au>
2026-07-21 10:12:53 +0930

    luInOut for ranking and summarising survey in/out categories

---
+++ data-raw/luInOut.R
@@ -0,0 +1,11 @@
+
+luInOut <- tibble::tribble(
+  ~in_out, ~rank
+  , "Time In, Space In", 1
+  , "Time Out, Space In", 2
+  , "Time In, Space Out", 3
+  , "Space Out", 4
+  , "Time Out, Space Out", 5
+  , "Not directly associated with site", 6
+  , "Opportune", 7
+)
